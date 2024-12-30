9370d45be9c3d4fffc7ceca8c638c46969ef3401
Pallav <iampallav698@gmail.com>
2024-12-31 00:57:51 +0530

    Second commit

--- force-app/main/default/lwc/feedbackForm/feedbackForm.js
+++ force-app/main/default/lwc/feedbackForm/feedbackForm.js
@@ -1,4 +1,5 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track, wire } from 'lwc';
+import getTrainigrecords from '@salesforce/apex/TrainingHandler.getTrainigrecords';
 
 export default class FeedbackForm extends LightningElement {
     ratingArray = [
@@ -8,6 +9,11 @@ export default class FeedbackForm extends LightningElement {
         { id: 4, iconName: 'utility:favorite_alt' },
         { id: 5, iconName: 'utility:favorite_alt' }
     ];
+
+    courseOptions = [];
+    displayCourseOptions = false;
+    @track courseOptions5 = [];
+    courseName = '';
 
     handleRatingChange(event) {
         const id = event.target.dataset.id;
@@ -29,5 +35,60 @@ export default class FeedbackForm extends LightningElement {
                 }
             })
         }
+    }
+
+    @wire(getTrainigrecords)
+    wiredTrainigRecords({ error, data }) {
+        if(data){
+            this.courseOptions = data.map(item => {
+                return {
+                    label: item.Name,
+                    value: item.Name
+                }
+            })
+
+            this.handleCourseOptions5();
+        }
+    }
+
+    handleCourseOptions5(val){
+        if(val === undefined){
+            this.courseOptions5 = this.courseOptions.slice(0, 5);
+        }
+        else{
+            this.courseOptions5 = this.courseOptions.filter(item => {
+                return item.label.toLowerCase().includes(val.toLowerCase());
+            })
+
+            if(this.courseOptions5.length > 5){
+                this.courseOptions5 = this.courseOptions5.slice(0, 5);
+            }
+        }
+    }
+
+    handleCourseChange(event){
+        const val = event.target.value;
+        console.log('Course: ' + val);
+        if(val !== ''){
+            this.displayCourseOptions = true;
+            this.handleCourseOptions5(val);
+            
+        }
+        else{
+            this.displayCourseOptions = false;
+        }
+        
+    }
+
+    handleComboFocus(){
+        this.displayCourseOptions = true;
+        this.handleCourseOptions5();
+        this.courseName = this.courseName === '' ? this.courseOptions[0].label : this.courseName;
+    }
+
+    handleCourseClick(event){
+        this.courseName = event.target.textContent;
+        this.displayCourseOptions = false;
+        console.log('Course Name: ' + this.courseName);
     }
 }
